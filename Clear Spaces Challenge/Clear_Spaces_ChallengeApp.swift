ab31724b74f701673c2bb2d328a7856beea76dc1
joogps <joogps@gmail.com>
2020-12-04 10:12:29 -0300

    Initial Commit

---
+++ Clear Spaces Challenge/Clear_Spaces_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clear_Spaces_ChallengeApp.swift
+//  Clear Spaces Challenge
+//
+//  Created by João Gabriel Pozzobon dos Santos on 04/12/20.
+//
+
+import SwiftUI
+
+@main
+struct Clear_Spaces_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clear Spaces Challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Clear Spaces Challenge
+//
+//  Created by João Gabriel Pozzobon dos Santos on 04/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
